2d5aa04cc7e213102f13eb65c8a55a0b066c4fa8
muskanrao <muskan.rao4347@gmail.com>
2022-09-10 16:56:48 +0530

    UPDATED FLODER

---
+++ WorkoutBee/Registration/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  WorkoutBee
+//
+//  Created by Muskan on 10/09/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ WorkoutBee/Registration/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  WorkoutBee
+//
+//  Created by Muskan on 10/09/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var RegisterButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func RegisterPressed(_ sender: UIButton) {
+    }
+    
+
+}
